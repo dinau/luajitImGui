69fd7172aea19a500fef5bbe5b2aa8f1017814cc
dinau <audgit@gmail.com>
2024-10-01 19:56:02 +0900

    changed: Resource files

--- examples/res.mk
+++ examples/res.mk
@@ -1,11 +1,14 @@
 RSC_NAME = rsc
 
-.PHONY: res
+.PHONY: res32 res64 clean png
 
-all: res
+all: clean res
 
-res: $(RSC_NAME)32.res $(RSC_NAME)64.res
+res: res64
 	@#png2ico  img/imguin.ico ../icon.png
+
+res32: $(RSC_NAME)32.res
+res64: $(RSC_NAME)64.res
 
 $(RSC_NAME)32.res: $(RSC_NAME).rc
 	windres -O coff $< -o $@
